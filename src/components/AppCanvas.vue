06874f1c8ff5f3292af3b011d8caaf8f5198cf52
stonesaw <mail.sou.dev@gmail.com>
2021-12-27 17:57:16 +0900

    add: p5 pkg and add life-game programs

---
+++ src/components/AppCanvas.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container border">
+    <div class="d-flex justify-content-center" id="p5Canvas"></div>
+    <div class="d-flex justify-content-center">
+      {{message}}
+    </div>
+  </div>
+</template>
+
+<script>
+const p5main = require('./AppCanvasP5proc')
+
+export default {
+  data() {
+    return {
+      message: ""
+    }
+  },
+
+  mounted() {
+    const P5 = require('p5')
+    new P5(p5main.main)
+    // NOTE: p5.jsからのコールバックを受け取る
+    // p5main.setDelegate(this.callbackOnP5);
+  },
+
+  methods: {
+    callbackOnP5: function(timeStr) {
+      this.message = timeStr;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
