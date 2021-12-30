e469ad8702fc60f81473b2da68acc0e32aa6d5b6
stonesaw <mail.sou.dev@gmail.com>
2021-12-30 18:57:13 +0900

    add: AppNavbar.vue & del: HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppNavbar />
     <AppCanvas />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNavbar from './components/AppNavbar.vue'
 import AppCanvas from './components/AppCanvas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    AppCanvas
+    AppNavbar,
+    AppCanvas,
   }
 }
 </script>
@@ -28,7 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 </style>
